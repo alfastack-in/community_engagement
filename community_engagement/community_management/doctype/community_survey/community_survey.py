f317ced605f20e22f76c9d06c60b2c09bd88996f
Aman Boora <aman.chaudharyboora@gmail.com>
2025-06-03 21:48:18 +0530

    App Created, Doctypes Created, with permission and users

---
+++ community_engagement/community_management/doctype/community_survey/community_survey.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, xyz and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CommunitySurvey(Document):
+	pass
